fb1d9879e6df942f9e7f8eea34fc650f83c61a02
nhatluudev <nhatluu031103@gmail.com>
2024-09-28 07:00:30 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -97,7 +97,7 @@ console.log('Telegram bot is runninggg...');
 app.use('', router);
 
 // Start the Express server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on porttt ${PORT}`);
 });
