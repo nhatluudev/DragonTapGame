fb6a42a1d0965bce27f7740b95c901f587cf5f29
nhatluudev <nhatluu031103@gmail.com>
2024-09-30 20:54:09 +0700

    update code

--- server/bot/telegram.js
+++ server/bot/telegram.js
@@ -1,32 +1,32 @@
-import TelegramBot from 'node-telegram-bot-api';
-import dotenv from 'dotenv';
+// import TelegramBot from 'node-telegram-bot-api';
+// import dotenv from 'dotenv';
 
-dotenv.config();
+// dotenv.config();
 
-const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true });
+// const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true });
 
-bot.onText(/\/start/, (msg) => {
-    console.log("FUCKINGSHIT")
-    console.log("TELEGRAM USER DATA")
-    const chatId = msg.chat.id;
-    const username = msg.from.username || 'Guest';
-    console.log(chatId)
-    console.log(username)
+// bot.onText(/\/start/, (msg) => {
+//     console.log("FUCKINGSHIT")
+//     console.log("TELEGRAM USER DATA")
+//     const chatId = msg.chat.id;
+//     const username = msg.from.username || 'Guest';
+//     console.log(chatId)
+//     console.log(username)
 
-    bot.sendMessage(chatId, `Hello, ${username}! Click the button below to launch the tapping game.`, {
-        reply_markup: {
-            inline_keyboard: [
-                [
-                    {
-                        text: 'Launch App',
-                        web_app: {
-                            url: process.env.NODE_ENV == 'production' ? process.env.NODE_CLIENT_ORIGIN : process.env.NODE_CLIENT_LOCAL_ORIGIN
-                        }
-                    }
-                ]
-            ]
-        }
-    });
-});
+//     bot.sendMessage(chatId, `Hello, ${username}! Click the button below to launch the tapping game.`, {
+//         reply_markup: {
+//             inline_keyboard: [
+//                 [
+//                     {
+//                         text: 'Launch App',
+//                         web_app: {
+//                             url: process.env.NODE_ENV == 'production' ? process.env.NODE_CLIENT_ORIGIN : process.env.NODE_CLIENT_LOCAL_ORIGIN
+//                         }
+//                     }
+//                 ]
+//             ]
+//         }
+//     });
+// });
 
-console.log('Telegram bot is running...');
+// console.log('Telegram bot is running...');
